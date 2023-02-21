cdb10c70d82c4b92911141eb458382eae3ffaca0
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-02-21 19:52:50 +0000

    Update navbar button to change according to page

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,23 +1,33 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
+	const router = useRouter();
+
+	const inStore = router.pathname.startsWith("/store");
+
 	return (
 		<nav className={classes.nav}>
-			<Link href="/" className={`${classes.logo} ${classes.link}`} >
+			<Link href="/" className={`${classes.logo} ${classes.link}`}>
 				<span></span>
 				<h1>Diddly Squat</h1>
 			</Link>
 			<ul className={classes.menu}>
 				<li>
-					<Link href="/about-us" className={classes.link}>About Us</Link>
+					<Link href="/about-us" className={classes.link}>
+						About Us
+					</Link>
 				</li>
 				<li>
-					<Link href="/contact-us" className={classes.link}>Contact Us</Link>
+					<Link href="/contact-us" className={classes.link}>
+						Contact Us
+					</Link>
 				</li>
-				<li className={classes.store}>
-					<Link href="/store" className={classes.link}>Store</Link>
-				</li>
+				<Link href="/store" className={classes.link}>
+					<li className={`${classes.store} ${classes.link}`}>{inStore ? "My Basket" : "Store"}</li>
+				</Link>
 			</ul>
 		</nav>
 	);
