ef2a10c772c768a0d0733284fb608406d29e3499
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-03-14 12:27:30 +0000

    Add animation to basket button

--- components/Layout/Navbar/BasketOverlay/BasketButton/BasketButton.tsx
+++ components/Layout/Navbar/BasketOverlay/BasketButton/BasketButton.tsx
@@ -1,19 +1,39 @@
 import { toggleCartVisibility } from "@/store/basketSlice";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
 import classes from "./BasketButton.module.css";
 
 const BasketButton: React.FC<{ numOfItems: number }> = (props) => {
+	const [buttonAnimation, setButtonAnimation] = useState(false);
 	const dispatch = useDispatch();
   
   const toggleBasketHandler = () => {
 		dispatch(toggleCartVisibility());
 	};
 
+	const buttonClasses = `${classes.button} ${buttonAnimation ? classes.bump : ""}`;
+
+	useEffect(() => {
+		if (props.numOfItems === 0){
+			return;
+		}
+
+		setButtonAnimation(true);
+
+		const timer = setTimeout(() => {
+      setButtonAnimation(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+	}, [props.numOfItems]);
+
 	return (
-		<button onClick={toggleBasketHandler} className={classes.button}>
+		<button onClick={toggleBasketHandler} className={buttonClasses}>
 			My Basket
-			{<span className={classes.badge}>{props.numOfItems}</span>}
+			<span className={classes.badge}>{props.numOfItems}</span>
 		</button>
 	);
 };
