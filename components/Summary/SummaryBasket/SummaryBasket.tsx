d5f2232056e339a945c6aadc3c21ecc864904340
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-03-21 18:58:36 +0000

    Add store button in the summary page

--- components/Summary/SummaryBasket/SummaryBasket.tsx
+++ components/Summary/SummaryBasket/SummaryBasket.tsx
@@ -3,13 +3,24 @@ import { useSelector } from "react-redux";
 import SummaryCategory from "./SummaryCategory/SummaryCategory";
 
 import classes from "./SummaryBasket.module.css";
+import { useRouter } from "next/router";
 
 const SummaryBasket = () => {
 	const categories = useSelector((state: RootState) => state.basket.categories);
+	const router = useRouter();
+
+	const storeButtonHandler = () => {
+		router.push("/store");
+	};
 
 	return (
-		<div style={{display: "grid", gap: "1em"}}>
-			<span className={classes.header}>My Basket</span>
+		<div style={{ display: "grid", gap: "1em" }}>
+			<div className={classes.header}>
+				<span className={classes.label}>My Basket</span>
+				<button className={classes.button} onClick={storeButtonHandler}>
+					Store
+				</button>
+			</div>
 			<ul className={classes["basket-container"]}>
 				{categories.map((category) => (
 					<SummaryCategory key={category.name} category={category} />
