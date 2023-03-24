b64ef535b6311b7336a8138acf5f5a476d28b2ed
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-03-24 17:26:23 +0000

    Rework category card

--- components/Categories/CategoryItem/CategoryItem.tsx
+++ components/Categories/CategoryItem/CategoryItem.tsx
@@ -1,37 +1,29 @@
 import Category from "@/models/category";
-import { toggleCartVisibility } from "@/store/basketSlice";
-import { RootState } from "@/store/store";
 import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
 import Image from "next/image";
 
 import classes from "./CategoryItem.module.css";
 
 const CategoryItem: React.FC<{ category: Category }> = (props) => {
 	const router = useRouter();
-	const showBasket = useSelector((state: RootState) => state.basket.showBasket);
-	const dispatch = useDispatch();
 
 	const goToCategory = () => {
 		router.push(`/store/${props.category.name.trim().toLowerCase()}`);
-		showBasket && setTimeout(() => {
-			dispatch(toggleCartVisibility());
-		}, 500);
 	};
 
 	return (
-		<li className={classes.wrapper}>
-			<button onClick={goToCategory} className={`${classes.card} ${classes.button}`}>
-			<Image
-				src={`/images/categories/${props.category.name.trim().toLowerCase()}.jpg`}
-				alt={props.category.image.alt}
-				className={classes.image}
-				width={2000}
-				height={2000}
-				quality={100}
-				priority
-			/>
-				<p className={classes.label}>{props.category.name}</p>
+		<li>
+			<button onClick={goToCategory} className={classes.category}>
+				<Image
+					src={`/images/categories/${props.category.name.trim().toLowerCase()}.jpg`}
+					alt={props.category.image.alt}
+					className={classes.category__image}
+					width={2000}
+					height={2000}
+					quality={100}
+					priority
+				/>
+				<span className={classes.category__label}>{props.category.name}</span>
 			</button>
 		</li>
 	);
