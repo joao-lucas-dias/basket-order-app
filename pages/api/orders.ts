2877de8e4a29f8241b8b6a1c600de32cde527bcb
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-03-28 12:54:24 +0100

    Update api route for submitting orders

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -1,10 +1,23 @@
+import { mongoDBConnectionString } from "@/secrets";
+import { MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
 // POST /api/orders
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const order = req.body;
-
   console.log(order);
+
+  const mongoClient = await MongoClient.connect(mongoDBConnectionString);
+  const db = mongoClient.db();
+
+  const ordersCollection = db.collection('orders');
+  const result = await ordersCollection.insertOne(order);
+
+  console.log(result);
+
+  mongoClient.close();
+
+  res.status(201).json({ message: 'Order registered successfully!'});
 };
 
 export default handler;
