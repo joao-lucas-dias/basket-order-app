5388215bb886fe43c1fdb930516d903b290dceed
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-03-29 17:15:02 +0100

    Change font to roboto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,20 @@ import type { AppProps } from "next/app";
 import Navbar from "@/components/Layout/Navbar/Navbar";
 import { Provider } from "react-redux";
 import store from "@/store/store";
+import { Roboto_Flex } from '@next/font/google';
+
+const roboto = Roboto_Flex({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
+			<style jsx global>
+				{`
+					html {
+						font-family: ${roboto.style.fontFamily};
+					}
+				`}
+			</style>
 			<Navbar />
 			<Component {...pageProps} />
 		</Provider>
